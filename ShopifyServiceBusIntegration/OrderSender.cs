6cef476944a704c03be971fc646cd887f195b076
Alex VanLaningham <ajvanlaningham@gmail.com>
2023-10-30 11:25:59 -0400

    initial commit

---
+++ ShopifyServiceBusIntegration/OrderSender.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ShopifyServiceBusIntegration
+{
+    public static class OrderSender
+    {
+        [FunctionName("OrderSender")]
+
+        [return: ServiceBus(queueOrTopicName: "orderqueue", Connection = "AzureWebJobsServiceBus")]
+
+        public static async Task<string> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("New Order placed in shopify storefront");
+
+            string shopifyStoreUrl = Environment.GetEnvironmentVariable("ShopURL");
+            string apiKey = Environment.GetEnvironmentVariable("APIKey");
+            string password = Environment.GetEnvironmentVariable("SecretKey");
+
+            string body = String.Empty;
+            using (var reader = new StreamReader(req.Body, Encoding.UTF8))
+            {
+                body = await OrderProcessTask(shopifyStoreUrl, password, apiKey, log);
+                log.LogInformation($"Message Body : {body}");
+            }
+
+            log.LogInformation($"SendMessage processed");
+            return body;
+        }
+
+    }
+}
