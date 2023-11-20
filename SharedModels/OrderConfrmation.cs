563ad3d213935646deb0b3cedd73b68921e420dc
Alex VanLaningham <ajvanlaningham@gmail.com>
2023-11-20 16:19:05 -0500

    adding Order Listener

---
+++ SharedModels/OrderConfrmation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedModels
+{
+    public class OrderConfirmation
+    {
+        [JsonPropertyName("site_id")]
+        public int SiteID { get; set; }
+        [JsonPropertyName("contact_id")]
+        public int ContactID { get; set; }
+        [JsonPropertyName("location_id")]
+        public int LocationID { get; set; }
+        [JsonPropertyName("order_number")]
+        public int OrderNumber { get; set; }
+        [JsonPropertyName("error_msg")]
+        public string? ErrorMsg { get; set; }
+    }
+}
